fcb635e9b164a40f05b80e71bd89f24b6bdc637c
barba3 <10989970+barba3@users.noreply.github.com>
2025-08-18 21:19:33 -0700

    Simple index with company name and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,13 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "BV Fullstack";
+export const SITE_DESCRIPTION = "BV Fullstack provides software design, development, and infrastructure services, from custom applications to cloud-native solutions.";
+
+export const company = {
+  name: "BV Fullstack",
+  fullName: "BV Fullstack LLC",
+  tagline: "From Core to Cloud",
+  description: `${SITE_DESCRIPTION}`,
+  contactEmail: "bvfullstack@gmail.com"
+};
